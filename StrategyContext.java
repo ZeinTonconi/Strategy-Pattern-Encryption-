15d517a10dffcffd80cc5879ab7f109914f09159
ZeinTonconi <102330309+ZeinTonconi@users.noreply.github.com>
2024-03-15 11:02:46 -0600

    Create StrategyContext.java

---
+++ StrategyContext.java
@@ -0,0 +1,23 @@
+
+public class StrategyContext implements EncryptionStrategy {
+    private EncryptionStrategy strategy;
+
+    public void setStrategy(EncryptionStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    @Override
+    public String encrypt(String texto) {
+        return strategy.encrypt(texto);
+    }
+
+    @Override
+    public String decrypt(String texto) {
+        return strategy.decrypt(texto);
+    }
+
+    @Override
+    public void init(Object secretKey) {
+        strategy.init(secretKey);
+    }
+
